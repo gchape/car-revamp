f7b70328e8a35c0e59768925a81debd9cb140d76
Giorgi Chapidze <chapidze.giorgi@proton.me>
2025-10-20 00:26:21 +0400

    Create Home layout with conditional Main component and footer
    - Added Home component layout with conditional rendering for the Main component on the root path.
    - Integrated LiveChat feature to be displayed on all pages.
    - Added Footer component with copyright and icon, ensuring it's visible on all pages.
    - Set up the layout structure with a flexible container, ensuring proper display of navbar, content, and footer.
    
    Improves the page structure by providing consistent navigation, footer, and chat support across the site.

---
+++ src/components/page/Home.tsx
@@ -0,0 +1,28 @@
+import { Outlet, useLocation } from "react-router";
+import LiveChat from "../../features/LiveChat";
+import Navbar from "../../features/Navbar";
+import Footer from "../Footer";
+import Main from "../Main";
+
+function Home() {
+  const location = useLocation();
+
+  return (
+    <div className="flex flex-col min-h-screen select-none">
+      <Navbar />
+      <LiveChat />
+
+      {location.pathname === "/" && (
+        <div className="flex-grow flex items-center justify-center">
+          <Main />
+        </div>
+      )}
+
+      <Outlet />
+
+      <Footer />
+    </div>
+  );
+}
+
+export default Home;
