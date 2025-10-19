f6d64ea4b18440c2c21dda39f44027e676452613
Giorgi Chapidze <chapidze.giorgi@proton.me>
2025-10-20 00:28:16 +0400

    Add Contact page with Google Maps embed and contact details
    - Created Contact page with Google Maps iframe showing the business location (ALEKO MOTORS).
    - Implemented a loading indicator that displays until the map is fully loaded.
    - Added company address, phone number, and working hours for easy customer access.
    - Optimized layout with responsive design for both mobile and desktop.
    - Utilized environment variable for the Google Maps API key to securely load the map.
    
    Enhances the contact experience with interactive map integration and clear contact information.

---
+++ src/components/page/Contact.tsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+
+const Contact = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  return (
+    <div className="flex flex-col grow-1 md:flex-row justify-center items-center gap-12 lg:gap-24 px-1.5 py-12 pb-5">
+      {isLoading && <span className="loading loading-dots loading-xl" />}
+
+      <iframe
+        className="w-full md:w-1/2 h-130 rounded-xl shadow-lg overflow-hidden relative"
+        style={{ border: "none" }}
+        loading="lazy"
+        allowFullScreen
+        onLoad={() => setIsLoading(false)}
+        src={`https://www.google.com/maps/embed/v1/place?key=${
+          import.meta.env.VITE_GOOGLE_MAPS_API_KEY
+        }&q=ALEKO+MOTORS,42.2351271,42.6917871`}
+      />
+
+      <div className="flex flex-col justify-center items-center md:items-start text-center md:text-left space-y-6 md:space-y-8 text-stone-300 font-arial-geo tracking-wider">
+        <h2 className="text-3xl font-semibold text-yellow-400">საკონტაქტო</h2>
+        <p className="text-lg md:text-xl font-medium font-nunito">
+          <span className="font-bold font-arial-geo">მისამართი:</span> 21 G
+          Nikea St, Kutaisi
+        </p>
+        <p className="text-lg md:text-xl font-medium font-nunito">
+          <span className="font-bold font-arial-geo">ტელეფონი:</span> +995
+          500-50-41-69
+        </p>
+        <p className="text-lg md:text-xl font-medium">
+          <span className="font-bold">სამუშაო საათები:</span> ორშ. - კვ. 10:00 -
+          18:00
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
